d5b0cb0e97a0c0684f7beca51d8cfaa562f21b21
Holger Martiker <info@einsatzverwaltung.de>
2020-01-08 16:17:02 +0100

    Bugfix ObjectExtension for Cloning

--- EmergencyDataExchangeProtocol/Models/ObjectExtensions.cs
+++ EmergencyDataExchangeProtocol/Models/ObjectExtensions.cs
@@ -1,4 +1,4 @@
-﻿using EmergencyDataExchangeProtocol.Models.ArrayExtensions;
+﻿using EmergencyDataExchangeProtocol.ArrayExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
